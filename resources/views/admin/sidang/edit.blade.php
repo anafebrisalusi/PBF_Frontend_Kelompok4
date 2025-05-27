011f49c7f2fea1122678ce0767ee4b67ab093702
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2025-05-27 10:39:23 +0700

    edit.blade.php

--- resources/views/admin/sidang/edit.blade.php
+++ resources/views/admin/sidang/edit.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <h3>Edit Jadwal Sidang</h3>
-    <form method="POST" action="/admin/sidang/{{ $sidang->id_sidang }}">
+    <form method="POST" action="/admin/sidang/{{ $sidang-['id_sidang'] }}">
         @csrf
         @method('PUT')
         @include('admin.sidang.form')
